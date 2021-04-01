45c62a4bb098b814707f1d314a8650d390c8ccee
Frank Runfola <Frunfola1229@gmail.com>
2021-04-01 16:56:24 -0400

    added new index nav ar collapse, added text containers with shadow, added a preloader on index page - looking good!

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const preloader = document.querySelector('.preloader');
+
+const fadeEffect = setInterval(() => {
+    if (!preloader.style.opacity) preloader.style.opacity = 1; // set opacity to be visible
+    if (preloader.style.opacity > 0) preloader.style.opacity -= 0.1;
+    else {
+        clearInterval(fadeEffect)
+    }
+    preloader.style.display = "none";
+    ;
+}, 200);
+
+window.addEventListener('load', fadeEffect);// when load event is added, run fadeEffect
