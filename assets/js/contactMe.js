b26118fe414f74240d495975baba7d4cf581a786
Frank Runfola <Frunfola1229@gmail.com>
2021-04-05 17:10:05 -0400

    EMAIL WORKSgit add . just ned to display message after form submit

--- assets/js/contactMe.js
+++ assets/js/contactMe.js
@@ -1,119 +1,31 @@
-let name = document.querySelector("#name").value;
-let email = document.querySelector("#email").vale;
-let phone = document.querySelector("#phone").value;
-let message = document.querySelector("#message").value;
-let firstName = name; // For Success/Failure Message;
-let status = document.querySelector(".status");
-let sendMessageButton = document.querySelector("#sendMessageButton");
-sendMessageButton.addEventListener("click", validateForm);
+let contactForm = document.querySelector("#contact-form")
 
-function validateForm() {
-    if (name === "") {
-        status.innerHTML = "Name cannot be empty";
-        return false;
+let submitHandler = e => {
+    let url = "/mail/contact_me.php"
+    let messages = document.querySelector("#message")
+    const formData = new FormData(e.target)
+    const formDataObject = Object.fromEntries(formData.entries())
+    const formDataString = JSON.stringify(formDataObject)
+
+    try {
+        console.log(`Begin submitHeader()...`)
+
+        fetch(url, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: formDataString
+        })
+            .then(res => { res.json(); console.log(`res.json()=${res.json()}`) })
+            .then(data => { messages.innerHTML += `Success: ${data}` })
+            .catch(err => { messages.innerHTML += `Fail: ${err}` })
+
+        console.log(`END submitHeader()...`)
     }
-    if (email === "") {
-       status.innerHTML = "Email cannot be empty";
-        return false;
-    } 
-    else {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if (!re.test(email)) {
-            status.innerHTML = "Email format invalid";
-            return false;
-        }
+    catch (err) {
+        console.log(`${err}`)
     }
-    if (phone === "") {
-        status.innerHTML = "Phone cannot be empty";
-        return false;
-    }
-    if (message == "") {
-       status.innerHTML = "Message cannot be empty";
-        return false;
-    }
-   status.innerHTML = "Sending...";
 }
 
-status.innerHTML = "Sending...";
-formData = {'name': name, 'email': email,'phone': phone,'message': message};
-
-$.ajax({
-    url: "/mail/contactMe.php",
-    type: "POST",
-    data: formData,
-    success: function (data, textStatus, jqXHR)
-    {
-        status.text(data.message);
-        if (data.code) //If mail was sent successfully, reset the form.
-            $('#contact-form').closest('form').find("input[type=text], textarea").val("");
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-        $('#status').text(jqXHR);
-    }
-});
-
-
-
-/**
-(function () {
-
-    ("#contactForm input, #contactForm textarea").jqBootstrapValidation(
-        {
-            preventSubmit: true,
-            submitError: ($form, event, errors) => { },  // additional error messages or events
-
-            submitSuccess: ($form, event) => {
-                event.preventDefault(); // prevent default submit behaviour
-
-                if (firstName.indexOf(' ') >= 0)  // Check for white space in name for Success/Fail message
-                    firstName = name.split(' ').slice(0, -1).join(' ');
-
-                sendMessageButton.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-                document.ajax(
-                    {
-                        url: "/mail/contact_me.php",
-                        type: "POST",
-                        data: { name: name, phone: phone, email: email, message: message },
-                        cache: false,
-
-                        success: () => {
-                            $('#success').html("<div class='alert alert-success'>"); // Success message
-                            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
-                            $('#success > .alert-success').append("<strong>Your message has been sent. </strong>");
-                            $('#success > .alert-success').append('</div>');
-                            $('#contactForm').trigger("reset");  //clear all fields
-                        },
-                        error: () => {
-                            $('#success').html("<div class='alert alert-danger'>"); // Fail message
-                            $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
-                            $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-                            $('#success > .alert-danger').append('</div>');
-                            $('#contactForm').trigger("reset");  //clear all fields
-                        },
-                        complete: () => {
-                            setTimeout(() => { document.prop("disabled", false); }, 1000); // Re-enable submit button when AJAX call is complete
-                        }
-                    });
-            },
-            filter: () => { return document.is(":visible"); },
-        });
-
-    document.querySelector("a[data-toggle=\"tab\"]").click((e) => {
-        e.preventDefault();
-        document.tab("show");
-    });
-});
-
-/*********************************************/
-/*When clicking on Full hide fail/success boxes */
-/*********************************************/
-
-/**
 window.onload = () => {
-    document.querySelector('#name').focus(() => {
-        document.querySelector('#success').html('');
-    })
-};
-
-
-*/
+    contactForm.addEventListener("submit", submitHandler)
+}
--- mail/contactMe.php
+++ mail/contactMe.php
@@ -1,59 +1,59 @@
 <?php
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+
+if (empty($_POST['name']) || empty($_POST['email']) ||  empty($_POST['message'])) {
+
+    echo var_dump($input);
+    http_response_code(500);
+    exit();
+}
+
+$input = json_decode(file_get_contents('php://input'), true);
+var_dump($input);
+
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-$subject = $_POST['subject'];
-header('Content-Type: application/json');
-if ($name === '')
-{
-    print json_encode(array(
-        'message' => 'Name cannot be empty',
-        'code' => 0
-    ));
-    exit();
-}
-if ($email === '')
-{
-    print json_encode(array(
-        'message' => 'Email cannot be empty',
-        'code' => 0
-    ));
-    exit();
-}
-else
-{
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL))
-    {
-        print json_encode(array(
-            'message' => 'Email format invalid.',
-            'code' => 0
-        ));
-        exit();
+$from = $email;
+$sendTo = 'rohini@biglittlebashes.com';
+$subject = 'New message from contact form'; // subject of the email;
+
+// form field names and their translations. array variable name => Text to appear in the email
+$fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message');
+$okMessage = 'Contact form successfully submitted.';
+$errorMessage = 'There was an error while submitting the form.';
+$emailText = "";
+
+try {
+    if (count($_POST) == 0)
+        throw new \Exception('Form is empty');
+
+    foreach ($_POST as $key => $value) {
+        if (isset($fields[$key])) // when field exists in the $fields array, include it in the email 
+            $emailText .= "$fields[$key]: $value\n";
     }
+
+    // All the necessary headers for the email.
+    $headers = array(
+        'Content-Type: text/plain; charset="UTF-8";',
+        'From: ' . $email,
+        'Reply-To: ' . $_POST['email'],
+        'Return-Path: ' . $email,
+    );
+
+    mail($sendTo, $subject, $emailText, implode("\n", $headers));   // Send email
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+} catch (\Exception $e) {
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
 }
-if ($subject === '')
-{
-    print json_encode(array(
-        'message' => 'Subject cannot be empty',
-        'code' => 0
-    ));
-    exit();
-}
-if ($message === '')
-{
-    print json_encode(array(
-        'message' => 'Message cannot be empty',
-        'code' => 0
-    ));
-    exit();
-}
-$content = "From: $name \nEmail: $email \nMessage: $message";
-$recipient = "youremail@here.com";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $content, $mailheader) or die("Error!");
-print json_encode(array(
-    'message' => 'Email successfully sent!',
-    'code' => 1
-));
-exit();
-?>
+
+
+// if requested by AJAX request return JSON response
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+    header('Content-Type: application/json');
+    echo $encoded;
+} else
+    echo $responseArray['message']; // else just display the message */
