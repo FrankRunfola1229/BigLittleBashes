e055220b36038018f21bbb8e0a7837baff423b27
Frank Runfola <frunfola1229@gmail.com>
2021-04-04 23:46:47 -0400

    Update contactMe.js

--- assets/js/contactMe.js
+++ assets/js/contactMe.js
@@ -1,43 +1,38 @@
-let name = document.querySelector("#name").value
-let email = document.querySelector("#email")
-let phone = document.querySelector("#phone")
-let message = document.querySelector("#message")
-let firstName = name; // For Success/Failure Message
+let name = document.querySelector("#name").value;
+let email = document.querySelector("#email").vale;
+let phone = document.querySelector("#phone").value;
+let message = document.querySelector("#message").value;
+let firstName = name; // For Success/Failure Message;
 let sendMessageButton = document.querySelector("#sendMessageButton");
-
-let submitButton = document.querySelector("#sendMessageButton")
-submitButton.addEventListener("click", validateForm)
+let submitButton = document.querySelector("#sendMessageButton");
+submitButton.addEventListener("click", validateForm);
 
 function validateForm() {
-    var name = document.getElementById('name').value;
-    if (name == "") {
+    if (name === "") {
         document.querySelector('.status').innerHTML = "Name cannot be empty";
         return false;
     }
-    var email = document.getElementById('email').value;
-    if (email == "") {
+    if (email === "") {
         document.querySelector('.status').innerHTML = "Email cannot be empty";
         return false;
-    } else {
+    } 
+    else {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if (!re.test(email)) {
             document.querySelector('.status').innerHTML = "Email format invalid";
             return false;
         }
     }
-    var subject = document.getElementById('phone').value;
-    if (subject == "") {
+    if (phone === "") {
         document.querySelector('.status').innerHTML = "Phone cannot be empty";
         return false;
     }
-    var message = document.getElementById('message').value;
     if (message == "") {
         document.querySelector('.status').innerHTML = "Message cannot be empty";
         return false;
     }
     document.querySelector('.status').innerHTML = "Sending...";
 }
-
 
 
 document.getElementById('status').innerHTML = "Sending...";
