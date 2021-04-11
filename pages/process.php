9f54670987f2cf1dc57220bdfc7e09c7ef92d90a
Frank Runfola <frunfola1229@gmail.com>
2021-04-11 16:23:22 -0400

    Update process.php

--- pages/process.php
+++ pages/process.php
@@ -21,7 +21,7 @@
         <p>
             While the planning process for each event is as unique as the event itself, we try to structure our
             process in the steps below. These are actively managed via Event Planning software within Honeybook.
-            Client are provided their own unique link to the workspace where they are able to view the status of
+            Clients are provided their own unique link to the workspace where they are able to view the status of
             their project and communication with vendors, and myself at any time. We also use Honeybook as our
             primary scheduler for meetings, consultations and site visits.
         </p>
